b7ed3242d4d09276d5effeaba6c7271ee8902611
XanDev <165346969+XanDev17@users.noreply.github.com>
2024-11-24 05:07:40 -0800

    Add files via upload

---
+++ src/phonebook/PhoneBook.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package phonebook;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author OBENG
+ */
+public class PhoneBook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        book.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
